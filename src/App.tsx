f44e147380b87f9930e980f122b48e13a0522897
Your Name~kunal-511 <yoyokvunal@gmail.com>
2023-09-20 21:33:11 +0530

    exact added

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { BrowserRouter as Router, Route, Routes  } from "react-router-dom"
-import Home from "./Components/Home"
-import About from "./Components/About"
-import Services from "./Components/Services"
-import ContactUs from "./Components/ContactUs"
-import Header from "./Components/Header"
-import Template1 from "./Components/Template1"
-import Template2 from "./Components/Tempelate2"
-import Footer from "./Components/Footer"
-function App() {
- 
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import ContactUs from "./Components/ContactUs";
+import Header from "./Components/Header";
+import Template1 from "./Components/Template1";
+import Template2 from "./Components/Tempelate2";
+import Footer from "./Components/Footer";
 
+function App() {
   return (
     <Router>
-           <Header />
-          
+      <Header />
       <Routes>
-        <Route path="/resume-builder/" element= {<Home />} />
-        <Route path="/about" element= {<About />} />
-        <Route path="/services" element= {<Services />} />
-        <Route path="/contact" element= {<ContactUs />} />
-        <Route path="/template1" element= {<Template1 />} />
-        <Route path="/template2" element= {<Template2 />} />
+        <Route path="/resume-builder" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/template1" element={<Template1 />} />
+        <Route path="/template2" element={<Template2 />} />
+        {/* The following route will match exactly "/resume-builder" */}
+        <Route
+          path="/"
+          element={<Navigate to="/resume-builder" replace />}
+        />
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
